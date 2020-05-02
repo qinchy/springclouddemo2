263d480828e11b6fe1c5225ad48f6832263c6cc7
qinchy <qinchy@msn.com>
2020-05-02 16:09:46 +0800

    qincy init

--- springclouddemo-provider1/src/main/java/com/qinchy/springclouddemoprovider2/controller/ConfigController.java
+++ springclouddemo-provider1/src/main/java/com/qinchy/springclouddemoprovider2/controller/ConfigController.java
@@ -4,6 +4,8 @@ package com.qinchy.springclouddemoprovider2.controller;
 import com.qinchy.springclouddemoprovider2.Constant.SeasonEnum;
 import com.qinchy.springclouddemoprovider2.Constant.WeekdayEnum;
 import com.qinchy.springclouddemoprovider2.domain.ConfigBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -11,8 +13,12 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class ConfigController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConfigController.class);
 
     @Autowired
     private ApplicationContext ctx;
@@ -39,11 +45,22 @@ public class ConfigController {
     @GetMapping("/config3")
     public String getName3() {
         if (1 == SeasonEnum.SPRING.getSeq()) {
-            System.out.println(SeasonEnum.SPRING.getSeq());
+            Objects.equals("aa", "aa");
+            logger.info(" SPRING VALUE = " + SeasonEnum.SPRING.getSeq());
         }
 
+        Integer a = 99;
+        Integer b = new Integer(99);
+        logger.info(" a == b?" + (a == b));
+
+        // IntegerCache类
+        Integer c = 129;
+        Integer d = new Integer(129);
+        logger.info("c == d?" + (c == d));
+        logger.info("c equals d?" + Objects.equals(c, d));
+
         WeekdayEnum.MONDAY.setValue(0);
-        System.out.println(WeekdayEnum.MONDAY.getValue());
+        logger.info("MONDAY VALUE = "+WeekdayEnum.MONDAY.getValue());
 
         return property;
     }
